2d962558179c36bdbd0c2ef03f3898f58e975c08
HGAS987 <141149310+HGAS987@users.noreply.github.com>
2025-08-12 10:51:33 +0200

    Create firebase-config.js

---
+++ firebase-config.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyANXzFCNDTrpMiv5nHd5shfXbrZ64VGzDM",
+  authDomain: "chat-tv-girl-864fe.firebaseapp.com",
+  projectId: "chat-tv-girl-864fe",
+  storageBucket: "chat-tv-girl-864fe.firebasestorage.app",
+  messagingSenderId: "797956573850",
+  appId: "1:797956573850:web:79f800acafb558542fed23",
+  databaseURL: "https://chat-tv-girl-864fe-default-rtdb.europe-west1.firebasedatabase.app/"
+};
+firebase.initializeApp(firebaseConfig);
